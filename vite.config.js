bc0bdd356ca5476e55b672b89a67eb9f50555292
Schneider <schneidercadet@live.fr>
2025-01-09 15:08:51 -0500

    Downgrade Vite to a more stable version

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-    commonjsOptions: {
-      include: [/node_modules/],
-      transformMixedEsModules: true
-    }
-  }
+  plugins: [react()]
 })
