9d020ea52f564b9bad02186206073fd98a19f90d
Schneider <schneidercadet@live.fr>
2025-01-09 14:53:54 -0500

    Update vite config and reinstall dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'],
+      output: {
+        globals: {
+          'react-router-dom': 'ReactRouterDOM'
+        }
+      }
+    }
+  }
 })
